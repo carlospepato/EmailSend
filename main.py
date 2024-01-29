d544b54c4697be8d7d1ef3eaae29d39f315a29a3
carlospepato <71358695+carlospepato@users.noreply.github.com>
2024-01-29 09:27:08 -0300

    finalizando a lógica

--- main.py
+++ main.py
@@ -4,35 +4,71 @@ import win32com.client
 from openpyxl import load_workbook
 
 
+def definir_estilo_tabela(tabela):
+    estilo_cabecalho = {
+        'selector': 'thead th',  # Seleciona todas as células do cabeçalho
+        'props': [('background-color', 'black'), ('color', 'white')]  # Define o fundo preto e texto branco
+    }
+
+    tabela_html = (
+        tabela.style
+        .set_table_styles([estilo_cabecalho])
+        .to_html(index=False, classes="table table-striped"))
+
+    return tabela_html
+
 def ler_planilha():
     remetente = str(input('email:'))
-    planilha = pd.read_excel(r'planilha_teste.xlsx')
+    planilha = pd.read_excel(r'./planilha/planilha_teste.xlsx')
     df = pd.DataFrame(planilha)
-    grupos_email = df.groupby("E-mail")
-    for email, grupo in grupos_email:
-        df_grupo = grupo[["Reference", "Currency", "Amount SAP"]]
-        print(email)
+    grupos_email = df.groupby("E-mail", group_keys=True)
 
-        # Convertendo o DataFrame para uma tabela HTML
-        tabela_html = df_grupo.to_html(index=False, border=1, classes="table table-bordered table-striped")
-        print(tabela_html)
+    for destinatario, grupo in grupos_email:
+        df_grupo = grupo[["Reference", "Currency", "Amount SAP"]]
+
+        # Formatação da coluna 'Amount SAP'
+        df_grupo = df_grupo.assign(**{'Amount SAP': df_grupo['Amount SAP'].apply(lambda x: '{:,.2f}'.format(x))})
+
+        print('Remetente:', type(remetente))
+        print('E-mail:', destinatario)
+
+        data = grupo['Data'].iloc[0]  # Assume que 'Data' é a mesma para todas as linhas do grupo
+        nome_empresa = grupo['Company'].iloc[0]  # Assume que 'Company' é a mesma para todas as linhas do grupo
+        moeda = grupo['Currency'].iloc[0]  # Assume que 'Currency' é a mesma para todas as linhas do grupo
+        valor = grupo['Amount SAP'].sum()
+
+        valor_formatado = '{:,.2f}'.format(-valor)
+
+        df_grupo = df_grupo.reset_index(drop=True)
+        tabela_html = '''
+                       <h4>Hello!</h4> 
+                        <p>How are you?</p>
+
+                        <p>We’d like to let you know that we are sending you today a wire transfer.</p>
+
+                        Wire transfer date: {0}<br/>
+                        To: {1}<br/>
+                        Total Amount: {2} {3}<br/>
+
+                        <p>Please see below the details of this payment:</p>
+                        '''.format(data, nome_empresa, moeda, valor_formatado)
+        tabela_html += definir_estilo_tabela(df_grupo)
 
         try:
-            enviar_emails(remetente, email, tabela_html)
-            print(f'E-mail enviado com sucesso para {email}')
+            enviar_emails(remetente, [destinatario], tabela_html)
+            print(f'E-mail enviado com sucesso para {destinatario}')
         except Exception as e:
-            print(f"Falha ao enviar e-mail para {email}. Erro: {str(e)}")
+            print(f"Falha ao enviar e-mail para {destinatario}. Erro: {str(e)}")
 
-    return remetente, email, tabela_html
+    #return remetente, destinatario, corpo1
 
 
 
-def obter_dados_email(planilha_excel):
-    workbook = load_workbook(planilha_excel)
+def obter_dados_email():
+    workbook = load_workbook(r'./planilha/planilha_teste.xlsx')
     sheet = workbook.active
 
-    remetente = sheet.cell(row=2, column=1).value  # Assume que o remetente está na célula (0, 0)
-    print(remetente)
+    remetente = sheet.cell(row=14, column=6).value  # Assume que o remetente está na célula (0, 0)
 
     destinatarios = []
     for row in range(2, sheet.max_row + 1):  # Começa da segunda linha para evitar o remetente
@@ -62,7 +98,7 @@ def enviar_emails(remetente, destinatarios, corpo):
 
 
 if __name__ == "__main__":
-    planilha_excel = "C:\\Users\\carlos.pepato\\Desktop\\teste_envio_email.xlsx"  # Substitua pelo caminho da sua planilha
+    """planilha_excel = "C:\\Users\\carlos.pepato\\Desktop\\teste_envio_email.xlsx"  # Substitua pelo caminho da sua planilha
     remetente, destinatarios = obter_dados_email(planilha_excel)
 
     if remetente and destinatarios:
@@ -74,5 +110,5 @@ if __name__ == "__main__":
         if not remetente:
             print("Remetente não encontrado na planilha.")
         if not destinatarios:
-            print("Destinatários não encontrados na planilha.")
-    #ler_planilha()
+            print("Destinatários não encontrados na planilha.")"""
+    ler_planilha()
